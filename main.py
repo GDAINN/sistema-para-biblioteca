347a9f144eaa4661048a0a717fafb6a2a4bfbfdc
GDAINN <coutinhodiogo482@gmail.com>
2026-04-03 14:42:55 -0300

    adicionando a class livro

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, HTTPException
-
+from pydantic import BaseModel
+from typing import Optional
 app = FastAPI()
 
 meu_dicionario = {}
+
+class Livro(BaseModel):
+    titulo: str
+    autor: str
+    ano: int
 
 @app.get("/livros")
 def get_livros():
@@ -12,25 +18,20 @@ def get_livros():
         return{"livros": meu_dicionario}
 
 @app.post("/adicionar")
-def post_livros(id: int, titulo: str, autor: str, ano: int):
+def post_livros(id: int, Livro: Livro):
     if id in meu_dicionario:
         raise HTTPException(status_code=400,detail="Esse livro já existe.")
     else:
-        meu_dicionario[id] = {"nome": titulo, "autor": autor, "ano": ano}
+        meu_dicionario[id] = Livro.dict()
         return {"message": "Livro foi criado"}
     
 @app.put("/atualizar/{id}")
-def put_livros(id: int, titulo: str, autor: str, ano: int):
+def put_livros(id: int, Livro: Livro):
     meu_livros = meu_dicionario.get(id)
     if not meu_livros:
         raise HTTPException(status_code=404, detail="Livro não encontrado.")
     else:
-        if titulo:
-            meu_livros["nome"] = titulo
-        if autor:
-            meu_livros["autor"] = autor
-        if ano:
-            meu_livros["ano"] = ano
+        meu_livros[id] = Livro.dict()
         
         return {"message": "Livro atualizado com sucesso."}
 
