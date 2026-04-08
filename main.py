ac55b13a5a5bcedcfc2763052f6389bc4ff42e94
GDAINN <coutinhodiogo482@gmail.com>
2026-04-08 10:20:14 -0300

    colocando usuario e senha no sistema

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 from typing import Optional
+import secrets
+import os 
+
 app = FastAPI()
+#criando um login para o minha api 
+meu_usuario = "admin"
+meu_senha = "admin123"
+
+security = HTTPBasic()
 
 meu_dicionario = {}
 
@@ -10,15 +19,22 @@ class Livro(BaseModel):
     autor: str
     ano: int
 
+
+def autenticar_meu_usuario(credentials: HTTPBasicCredentials = Depends(security)):
+    is_username_correct = secrets.compare_digest(credentials.username, meu_usuario)
+    is_password_correct= secrets.compare_digest(credentials.password, meu_senha)
+    if not (is_username_correct and is_password_correct):
+        raise HTTPException(status_code=401, detail="Credenciais inválidas.", headers={"WWW-Authenticate": "Basic"})
+
 @app.get("/livros")
-def get_livros():
+def get_livros(credentials: HTTPBasicCredentials = Depends(autenticar_meu_usuario)):
     if not meu_dicionario:
         return {"message": "Nenhum livro encontrado."}
     else:
         return{"livros": meu_dicionario}
 
 @app.post("/adicionar")
-def post_livros(id: int, Livro: Livro):
+def post_livros(id: int, Livro: Livro, credentials: HTTPBasicCredentials = Depends(autenticar_meu_usuario)):
     if id in meu_dicionario:
         raise HTTPException(status_code=400,detail="Esse livro já existe.")
     else:
@@ -26,7 +42,7 @@ def post_livros(id: int, Livro: Livro):
         return {"message": "Livro foi criado"}
     
 @app.put("/atualizar/{id}")
-def put_livros(id: int, Livro: Livro):
+def put_livros(id: int, Livro: Livro, credentials: HTTPBasicCredentials = Depends(autenticar_meu_usuario)):
     meu_livros = meu_dicionario.get(id)
     if not meu_livros:
         raise HTTPException(status_code=404, detail="Livro não encontrado.")
